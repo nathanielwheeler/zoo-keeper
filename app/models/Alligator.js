305b9a491fdc5827f6a0738c0267510c0a34f131
Porter Wilcox <wilcox.porter@gmail.com>
2018-08-06 14:46:58 -0600

    all-the-animals

---
+++ app/models/Alligator.js
@@ -0,0 +1,23 @@
+class Alligator {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+        weight = 0,
+        tailLengthInch = 0,
+        teethSize = '',
+    ) {
+        this.id = 0,
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+        this.weight = weight;
+        this.tailLengthInch = tailLengthInch;
+        this.teethSize = teethSize;
+    }
+    eat(food){
+        console.log(`Chomp chomp all the ${food}ies.`)
+    }
+}
+
+export default Alligator;
