dd0d81b3e7737a4c09a212cb5dd287f527488f69
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-08-19 14:56:04 -0600

    Changed syntax of alligator to accept objects

--- app/main.js
+++ app/main.js
@@ -1,3 +1,10 @@
 import Alligator from "./models/Alligator.js";
 
-let ally = new Alligator("Ally", "Green", "F", 200, 60, "big");
+let ally = new Alligator({
+    name: "Ally",
+    color: 'green',
+    gender: 'F',
+    weight: 200,
+    tailLengthInch: 60,
+    teethSize: "big",
+})
--- app/models/Alligator.js
+++ app/models/Alligator.js
@@ -1,21 +1,14 @@
 class Alligator {
-    constructor(
-        name = '',
-        color = '',
-        gender = '',
-        weight = 0,
-        tailLengthInch = 0,
-        teethSize = '',
-    ) {
-        this.id = 0,
-        this.name = name;
-        this.color = color;
-        this.gender = gender;
-        this.weight = weight;
-        this.tailLengthInch = tailLengthInch;
-        this.teethSize = teethSize;
+    constructor(data) {
+        this.id = 0;
+        this.name = data.name || null;
+        this.color = data.color || null;
+        this.gender = data.gender || null;
+        this.weight = data.weight || null;
+        this.tailLengthInch = data.tailLengthInch || null;
+        this.teethSize = data.teethSize || null;
     }
-    eat(food){
+    eat(food) {
         console.log(`Chomp chomp all the ${food}ies.`)
     }
 }
